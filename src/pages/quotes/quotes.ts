6ca881f9c93623990043244e2510e27d69a1255e
Chris Roche <chrisroche02@hotmail.com>
2018-07-28 13:16:25 +0100

    Favourite Alert

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 
 @Component({
@@ -8,7 +8,7 @@ import { Quote } from '../../data/quote.interface';
 })
 export class QuotesPage implements OnInit  {
   quoteGroup: { category: string, quotes: Quote[], icon: string };
-  constructor(private navParams: NavParams) {}
+  constructor(private navParams: NavParams, private alertCtrl: AlertController) {}
 
   //ionViewDidLoad() {
   //  this.quoteGroup = this.navParams.data;    
@@ -16,6 +16,31 @@ export class QuotesPage implements OnInit  {
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddFavourite(selectedQuote: Quote) {
+    const alert =  this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure you want to favourite this quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled!');
+          }
+        }
+      ]
+    });
+
+    alert.present();
+
   }
 
 }
